ed077bafc1144497c1ba440c477b8de527b6ea11
1yoursource <leo00pobeda@gmail.com>
2020-05-03 20:20:06 +0300

    add poiters changed nesting of structures to nesting pointers add reciever methods

--- hometask1_29042020.go
+++ hometask1_29042020.go
@@ -2,124 +2,200 @@ package main
 
 import (
 	"fmt"
-
 	log "github.com/vigo5190/goimports-example/b"
 )
 
 type person struct {
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
-	Age     int    `json:"age"`
+	Name        string `json:"name"`
+	Surname     string `json:"surname"`
+	Nationality string `json:"nationality"`
+	Sex         string `json:"sex"`
+	Age         uint8  `json:"age"`
+	Height      uint8  `json:"height"`
+	Weight      uint8  `json:"weight"`
 }
-
 type employee struct {
 	Salary float32 `json:"salary"`
-	person
+	person *person
 }
-
 type teacher struct {
 	Specialization string `json:"specialization"`
 	FormMaster     bool   `json:"form_master"`
-	employee
+	employee       *employee
 }
-
 type driver struct {
 	Category   string `json:"category"`
 	Experience uint8  `json:"experience"`
-	employee
+	employee   *employee
 }
-
 type hairdresser struct {
 	Certificate bool `json:"certificate"`
-	employee
+	employee    *employee
 }
-
 type psychologist struct {
 	License        bool   `json:"license"`
 	AcademicDegree string `json:"academic_degree"`
-	employee
+	employee       *employee
 }
 type electrician struct {
 	Category uint `json:"category"`
-	employee
+	employee *employee
 }
 type lawyer struct {
 	Direction string `json:"direction"`
 	License   bool   `json:"license"`
-	employee
+	employee  *employee
+}
+
+func (receiver *person) GetName() string {
+	fmt.Println("\nPerson name: ")
+	return receiver.Name
+	//return "\nPerson name: " + receiver.Name
+}
+func (receiver *person) GetSurname() string {
+	fmt.Println("\nPerson surname: ")
+	return receiver.Surname
+}
+func (receiver *person) GetNationality() string {
+	fmt.Println("\nPerson nationality: ")
+	return receiver.Nationality
+}
+func (receiver *person) GetSex() string {
+	fmt.Println("\nPerson sex: ")
+	return receiver.Sex
+}
+func (receiver *person) GetAge() uint8 {
+	fmt.Println("\nPerson age: ")
+	return receiver.Age
+}
+func (receiver *person) GetHeight() uint8 {
+	fmt.Println("\nPerson height: ")
+	return receiver.Height
+}
+func (receiver *person) GetWeight() uint8 {
+	fmt.Println("\nPerson weight: ")
+	return receiver.Weight
 }
 
 func main() {
-	var testTeacher = teacher{
-		Specialization: "math",
+	var testTeacher *teacher = &teacher{
+		Specialization: "Math",
 		FormMaster:     false,
-		employee: employee{
+		employee: &employee{
 			Salary: 9200.1,
-			person: person{
-				Name:    "testTeacherName",
-				Surname: "testTeacherSurname",
-				Age:     23,
+			person: &person{
+				Name:        "testTeacherName",
+				Surname:     "testTeacherSurname",
+				Nationality: "German",
+				Sex:         "Female",
+				Age:         23,
+				Weight:      65,
+				Height:      173,
 			},
 		},
 	}
-	var testDriver = driver{
+	var testDriver *driver = &driver{
 		Category:   "B",
 		Experience: 5,
-		employee: employee{
+		employee: &employee{
 			Salary: 9000.9,
-			person: person{
-				Name: "testDriverName",
-				Age:  30,
+			person: &person{
+				Name:        "testDriverName",
+				Surname:     "testDriverSurame",
+				Nationality: "Brazilian",
+				Sex:         "Male",
+				Age:         30,
+				Weight:      95,
+				Height:      180,
 			},
 		},
 	}
-	var testHairdress = hairdresser{
+	var testHairdress *hairdresser = &hairdresser{
 		Certificate: true,
-		employee: employee{
+		employee: &employee{
 			Salary: 14232.1,
-			person: person{
-				Name: "testHairdressName",
+			person: &person{
+				Name:        "testHairdressName",
+				Surname:     "testHairdressSurname",
+				Nationality: "Ukrainian",
+				Sex:         "Male",
+				Age:         22,
+				Weight:      75,
+				Height:      175,
 			},
 		},
 	}
-	var testPsyhologist = psychologist{
+	var testPsyhologist *psychologist = &psychologist{
 		License: true,
-		employee: employee{
+		employee: &employee{
 			Salary: 11000.1,
-			person: person{
-				Age:     33,
-				Name:    "testPsyhName",
-				Surname: "testPsyhSurname",
+			person: &person{
+				Name:        "testPsyhologistName",
+				Surname:     "testPsyhologistSurname",
+				Nationality: "Poland",
+				Sex:         "Male",
+				Age:         33,
+				Weight:      84,
+				Height:      179,
 			},
 		},
 	}
-	var testElectrician = electrician{
+	var testElectrician *electrician = &electrician{
 		Category: 4,
-		employee: employee{
+		employee: &employee{
 			Salary: 11000.35,
-			person: person{
-				Name: "testNameEl",
+			person: &person{
+				Name:        "testNameElectrician",
+				Surname:     "testSurnameameElectrician",
+				Nationality: "Belgium",
+				Sex:         "Male",
+				Age:         41,
+				Weight:      75,
+				Height:      180,
 			},
 		},
 	}
-	var testLawyer = lawyer{
+	var testLawyer *lawyer = &lawyer{
 		Direction: "Criminal Law",
-		employee: employee{
+		employee: &employee{
 			Salary: 33000.9,
-			person: person{
-				Age:  26,
-				Name: "testLawyerName",
+			person: &person{
+				Name:        "testLawyerName",
+				Surname:     "testLawyerSurname",
+				Nationality: "Ukrainian",
+				Sex:         "Female",
+				Age:         26,
+				Weight:      62,
+				Height:      174,
 			},
 		},
 	}
+
 	fmt.Println("Start")
 
-	fmt.Println("teacher name: ", testTeacher.Name)
-	fmt.Println("Driver Age: ", testDriver.Age)
-	fmt.Println("electrician salary: ", testElectrician.Salary)
-	fmt.Println("hairdress certificate: ", testHairdress.Certificate)
-	fmt.Println("Psyhologist surname: ", testPsyhologist.Surname)
-	fmt.Println("lawyer direction: ", testLawyer.Direction)
+	fmt.Println("teacher name: ", (*testTeacher).employee.person.Name)
+	fmt.Println("Driver Age: ", (*testDriver).employee.person.Age)
+	fmt.Println("electrician salary: ", (*testElectrician).employee.Salary)
+	fmt.Println("hairdress certificate: ", (*testHairdress).Certificate)
+	fmt.Println("Psyhologist surname: ", (*testPsyhologist).employee.person.Surname)
+	fmt.Println("lawyer direction: ", (*testLawyer).Direction)
 
-	fmt.Println("result of import package", log.Foo)
+	fmt.Println("result of import package: ", log.Foo)
+
+	fmt.Println("Start 2")
+	fmt.Println((*person).GetName((*testLawyer).employee.person))
+	fmt.Println((*person).GetSurname((*testPsyhologist).employee.person))
+	fmt.Println((*person).GetNationality((*testElectrician).employee.person))
+	fmt.Println((*person).GetSex((*testDriver).employee.person))
+	fmt.Println((*person).GetAge((*testTeacher).employee.person))
+	fmt.Println((*person).GetHeight((*testHairdress).employee.person))
+	fmt.Println((*person).GetWeight((*testHairdress).employee.person))
+
+	/*panic*/
+	if (*person).GetAge((*testLawyer).employee.person) == 26 {
+		f_panic()
+	}
+}
+func f_panic() {
+	panic("It`s panic!")
 }
